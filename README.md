1da96af16baa6e9c40fc173399bf1034a35d52fb
chiracmichoagan <michoaganchiracdona@gmail.com>
2026-02-22 23:32:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# 🌡 Page d'accueil  Medical Healthcare
+
+Cette page d'accueil, créée avec le framework TailwindCSS, est conçue pour les cabinets médicaux et vise à offrir une expérience positive aux professionnels de santé et à leurs patients.
+
+![Capture du projet](/assets/img/readme/Capture.JPG)
+
+### 🎉 Déploiement
+
+[Lien de déploiement](https://insove-medical-healthcare.netlify.app/)
+
+### 🔨 Technologies utilisées
+
+- `TailwindCSS` - [TailwindCSS](https://tailwindcss.com/)
+
+- `Remix Icons` - [Remix Icons](https://www.jsdelivr.com/package/npm/remixicon/)
+
+- `SwiperJS` - [Swiper](https://swiperjs.com/)
+
+- `Scroll Reveal` - [Scroll Reveal](https://scrollrevealjs.org/)
+
+### 🎨 Suggestion de design
+
+[Design] [Suggestion](https://templatesjungle.com/downloads/insove-medical-healthcare-free-figma-website-template/)
+
+### Ressources supplémentaires
+
+- **Navigation intuitive**
+
+- **Conception adaptative**
+
+- **Éléments interactifs**
+
+## Comment exécuter le projet
+
+1. Clonez le dépôt sur votre machine locale :
+
+``bash
+git clone https://github.com/nathanmota-dev/landing-page-insove-medical-healthcare
+
+``
+
+2. Accédez au répertoire du projet :
+
+``bash
+cd landing-page-insove-medical-healthcare
+
+``
+
+3. Installez les dépendances :
+
+``bash
+npm install
+
+```
+
+4. Démarrez le serveur de développement :
+
+``bash
+npm run watch
+
+```
+
+5. Ouvrez votre navigateur et accédez au serveur en ligne. [http://127.0.0.1:5500/](http://127.0.0.1:5500/) pour suivre l'avancement du projet.
+
+## Contributions
+
+Les contributions sont les bienvenues ! N'hésitez pas à ouvrir des tickets ou des demandes de fusion pour améliorer ce projet.
+
+## Licence
+
+Ce projet est distribué sous licence MIT.
---
+++ js/script.js
@@ -0,0 +1,117 @@
+//Mobile menu
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
+
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
+
+closeBtn.addEventListener('click', () => {
+    if (mobileNav.classList.contains(navOpenedClass)) {
+        mobileNav.classList.remove(navOpenedClass);
+        mobileNav.classList.add(navClosedClass);
+        closeBtnIcon.classList.remove(arrowLeftClass);
+        closeBtnIcon.classList.add(arrowRightClass);
+    } else {
+        mobileNav.classList.remove(navClosedClass);
+        mobileNav.classList.add(navOpenedClass);
+        closeBtnIcon.classList.remove(arrowRightClass);
+        closeBtnIcon.classList.add(arrowLeftClass);
+    }
+});
+
+
+
+//swiper.js - depoiments
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+  // FAQ 
+const faqItems = document.querySelectorAll('.faqItem');
+
+faqItems.forEach((item) => {
+    const faqBtn = item.querySelector('.faqBtn');
+    const faqText = item.querySelector('.faqText');
+
+    item.addEventListener('click', () => {
+        const isOpen = item.classList.toggle('open');
+        const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
+        const iconElement = faqBtn.querySelector('i');
+        iconElement.classList = `${iconClass} text-2xl`;
+
+        if (isOpen) {
+            faqText.style.height = `${faqText.scrollHeight}px`;
+        } else {
+            faqText.style.height = '0px';
+        }
+    });
+});
+
+//Scroll animations
+
+const sr = ScrollReveal({
+
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+});
+
+sr.reveal('.heroText'), { origin: 'top' };
+sr.reveal('.heroImg');
+
+sr.reveal('.statsItem', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'top',
+});
+
+sr.reveal('.services');
+sr.reveal('.serviceTop');
+sr.reveal('.servicesItem', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+sr.reveal('.appointmentTitle');
+sr.reveal('appointmentForm');
+
+sr.reveal('.testimonials');
+sr.reveal('.testimonialContainer');
+
+sr.reveal('.faqTitle');
+sr.reveal('.faqItem', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
+
+sr.reveal('.newsletter');
+sr.reveal('.newsletterContainer');
+
+sr.reveal('.footerItem', {
+    delay: 600,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom',
+});
